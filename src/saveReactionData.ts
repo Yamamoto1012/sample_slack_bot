9ea266f141a26afe5eb5ee392ad596570e3d6856
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2024-10-24 18:17:07 +0900

    feat: データベースにデータを保存する関数の作成

--- src/saveReactionData.ts
+++ src/saveReactionData.ts
@@ -14,5 +14,37 @@ async function saveReactionData(userId: string, messageId: string, reactionId: s
   }
 
   // messageテーブルの更新
-  
+  let { data: message, error: messageError } = await supabase
+  .from('message')
+  .upsert({ id: messageId }, { onConflict: 'id' });
+
+  if (messageError) {
+    console.error(messageError);
+    return;
+  }
+
+  // reactionテーブルの更新
+  let { data: reaction, error: reactionError } = await supabase
+  .from('reaction')
+  .upsert(
+    { id: reactionId, emoji_name: emojiName, created_at: userId },
+    { onConflict: 'id' })
+
+  if (reactionError) {
+    console.error(reactionError);
+    return;
+  }
+
+  // message_reactionテーブルの更新
+  let {error: massageReactionError} = await supabase
+  .from('messagereaction')
+  .insert(
+    { message_id: messageId, user_id: userId, reaction_id: reactionId, created_at: createdAt }
+  )
+
+  if (massageReactionError) {
+    console.error(massageReactionError);
+    return;
+  }
+  console.log('リアクションデータを保存しました');
 }
