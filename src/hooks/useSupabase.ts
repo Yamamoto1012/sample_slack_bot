cc8bfb775f10e56941529af35055c3ac48bfd56e
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2024-10-24 16:56:15 +0900

    fix: 環境変数を読み込めるように、ts-dotenvを使用

--- src/hooks/useSupabase.ts
+++ src/hooks/useSupabase.ts
@@ -6,16 +6,23 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 const env = load({
-  
+  SUPABASE_URL: {
+    type: String,
+    default: process.env.SUPABASE_URL || '',
+  },
+  SUPABASE_ANON_KEY: {
+    type: String,
+    default: process.env.SUPABASE_ANON_KEY || ''
+  }
 })
 
 export const useSupabase = () => {
-  if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
+  if (!env.SUPABASE_URL || !env.SUPABASE_ANON_KEY) {
     throw new Error('Supabaseの環境変数に問題があります')
   }
 
   // 環境変数や型ファイルを適用したクライアントを作成
-  const supabase = createClient<Database>(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY, {
+  const supabase = createClient<Database>(env.SUPABASE_URL, env.SUPABASE_ANON_KEY, {
     auth: {
       persistSession: false,
       autoRefreshToken: false,
